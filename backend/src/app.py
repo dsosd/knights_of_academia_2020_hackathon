a5f036bae7ddb78f5507d2220d659801d5a9be62
Rohan Patwardhan <HaroldTheSenpai@gmail.com>
2020-03-19 18:33:06 +0000

    Documentation, better ports
    Added comments and now the program looks for the arduino ports instead of using hardcoded ones.

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,26 +1,41 @@
 from flask import Flask, request, jsonify
 from multiprocessing import Process, Value
 import serial
-import time
+from serial.tools import list_ports
 import json
+import time
+
+# List of ports
+ports = list(list_ports.comports())
+
+# Look for the arduino port
+for p in ports:
+    # Print ports for debugging
+    print(p)
+    if "Arduino" in p.description:
+        print(f"Arduino Port -> {p}")
+        port = p
 
 plugged_in = False
 
+# Check if Arduino is plugged in
 while not plugged_in:
     try:
-        ser = serial.Serial("COM3", 9600, timeout = 0)
+        ser = serial.Serial(p, 9600, timeout = 0)
         plugged_in = True
     except:
         print("!!! NOT PLUGGED IN !!!")
         time.sleep(1)
 
+# Create server
 server = Flask(__name__)
 
 #? For hosting data
 @server.route('/')
 def index():
-    return jsonify(example_json)
+    return data
 
+# Serial loop for reading serial ports
 def serial_loop():
     print ("Entering main server loop..")
     while True:
@@ -30,6 +45,7 @@ def serial_loop():
         except ser.SerialTimeoutExeption:
             print("Data could not be read")
 
+# Run the server and the serial loop on a different process
 if __name__ == '__main__':
     p = Process(target=serial_loop)
     p.start()  
