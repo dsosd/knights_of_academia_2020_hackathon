6310f2dcc8e8230eeb405a5b779ae1ddb68c5d8d
Andy Huang <huang-a@users.noreply.github.com>
2020-03-20 20:22:07 -0400

    Refactor app.py a bit

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,30 +3,36 @@ import json
 from multiprocessing import Process, Value
 import serial
 from serial.tools import list_ports
+import sys
 import time
 
-# List of ports
-ports = list(list_ports.comports())
+def serial():
+    # List of ports
+    ports = list(list_ports.comports())
 
-# Look for the arduino port
-for p in ports:
-    # Print ports for debugging
-    print(f"{p}. ")
-    if "Arduino" in p.description:
-        print(f"Arduino Port -> {p}")
-        port = p
+    # Look for the arduino port
+    for p in ports:
+        # Print ports for debugging
+        print(f"{p}. ")
+        if "Arduino" in p.description:
+            print(f"Arduino Port -> {p}")
+            port = p
 
-plugged_in = False
+    plugged_in = False
+    port_num = 9600
 
-# Check if Arduino is plugged in
-while not plugged_in:
-    try:
-        ser = serial.Serial(p, 9600, timeout = 0)
-        plugged_in = True
-    except:
-        print("!!! NOT PLUGGED IN !!!")
-        #TODO add wait for keypress prompt instead
-        time.sleep(1)
+    # Check if Arduino is plugged in
+    while not plugged_in:
+        try:
+            #ser = serial.Serial(p, 9600, timeout = 0)
+            ser = serial.Serial(port, port_num, timeout = 0)
+            plugged_in = True
+        except:
+            print("!!! NOT PLUGGED IN !!!")
+            #TODO add wait for keypress prompt instead
+            time.sleep(1)
+
+    serial_loop()
 
 # Create server
 server = Flask(__name__)
@@ -38,17 +44,20 @@ def index():
 
 # Serial loop for reading serial ports
 def serial_loop():
-    print("Entering main server loop..")
+    print("Entering serial loop...")
     while True:
         #? Serial port reading
         try:
             data = dumps(ser.readline())
-        except ser.SerialTimeoutExeption:
-            print("Data could not be read")
+        except serial.SerialTimeoutExeption:
+            print("Data could not be read (serial timeout)")
 
 # Run the server and the serial loop on a different process
-if __name__ == "__main__":
-    p = Process(target = serial_loop)
-    p.start()
+def main()
+    proc = Process(target = serial)
+    proc.start()
     server.run(debug = True, use_reloader = False)
-    p.join()
+    proc.join()
+
+if __name__ == "__main__":
+    main()
