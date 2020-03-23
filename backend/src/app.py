a4873218c7444eb7ca6c6ea9201d880ab8a25a54
Andy Huang <huang-a@users.noreply.github.com>
2020-03-23 10:05:28 -0400

    Add websocket support +
    Finish http part of backend +
    
    This part has also been tested via curl.
    
    Add comments and TODOs +
    
    Add skeleton functions where there are TODOs +

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,6 @@
 import copy
 import flask
+import flask_socketio as flask_sio
 import json
 import multiprocessing as mproc
 import serial
@@ -50,17 +51,21 @@ def gen_400_err():
     return "Bad request made", 400
 
 def run_server():
-    server = flask.Flask(__name__)
+    port = 8080
+
+    http_server = flask.Flask(__name__)
+    ws_server = flask_sio.SocketIO(http_server)
 
     port_to_id = {}
     data = {}
+    timer = {}
 
     # HTTP part
-    @server.route("/api/ports", methods = ["GET"])
+    @http_server.route("/api/ports", methods = ["GET"])
     def ports_handler():
         return json.dumps(list(port_to_id.keys()))
 
-    @server.route("/api/connect", methods = ["GET"])
+    @http_server.route("/api/connect", methods = ["GET"])
     def connect_handler():
         try:
             return json.dumps(str(port_to_id[
@@ -69,7 +74,7 @@ def run_server():
         except KeyError:
             return gen_400_err()
 
-    @server.route("/api/board/<int:id>", methods = ["GET"])
+    @http_server.route("/api/board/<int:id>", methods = ["GET"])
     def board_handler(id):
         try:
             ret = copy.deepcopy(data[id])
@@ -79,7 +84,72 @@ def run_server():
         except KeyError:
             return gen_400_err()
 
-    server.run(debug = True, use_reloader = False)
+    # adds each relevant client, the frontend and the
+    # arduino part of the backend to their appropriate rooms
+    @ws_server.on("message")
+    def add_to_room(room_name):
+        # valid room names are "frontend" and "arduino"
+        flask_sio.join(room_name)
+
+    def frontend_ws_handler(payload):
+        json_data = json.loads(payload)
+
+        id = json_data["id"]
+        event = json_data["event"]
+
+        if event == "UPDATE_LEDS":
+            data[id]["leds"].update(json_data["data"])
+            # update the leds on the arduino side
+            ret = {
+                "event": "UPDATE_LEDS",
+                "data": {
+                    "leds": data[id]["leds"]
+                }
+            }
+            return data[id], "arduino"
+        elif event == "UPDATE_KEYBINDINGS":
+            data[id]["keybindings"].update(json_data["data"])
+            return {}
+        else:
+            return {"error": "Event is not valid"}
+
+    #TODO implement websocket support to arduino part of backend. simplifies design
+    def arduino_ws_handler(payload):
+        pass
+
+    #TODO timer functions need more specification. what triggers a timer??
+    def on_timer_start():
+        pass
+
+    def on_timer_end():
+        pass
+
+    #NOTE unknown rate of updates
+    def on_timer_update():
+        pass
+
+    # manages all inbound json payloads
+    @ws_server.on("json")
+    def json_handler(payload):
+        room = flask.session.get("room")
+
+        def reply(data, room = room):
+            emit("json", data, room = room, include_self = false)
+
+        if room == "frontend":
+            ret = frontend_ws_handler(payload)
+            if len(ret):
+                reply(ret)
+        elif room == "arduino":
+            ret = arduino_ws_handler(payload)
+            if len(ret):
+                reply(ret)
+        else:
+            reply({"error": "Room is not valid"})
+
+    # ws_server.run(...) starts the http server as well. comment is for debugging purposes
+    #http_server.run(port = port, debug = True, use_reloader = False)
+    ws_server.run(app = http_server, port = port, debug = True, use_reloader = False)
 
 # Run the http/ws server and the serial port interface on different processes
 def main():
